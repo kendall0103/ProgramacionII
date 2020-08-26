9897033702f368d2a23f0370a83f9f6ee3421506
Kendall Tencio Paniagua <kendal.tencio.paniagua@est.una.ac.cr>
2020-08-26 17:43:55 -0600

    avancee
    sirva porfa

--- Proyecto/Proyecto/Principal.cpp
+++ Proyecto/Proyecto/Principal.cpp
@@ -3,7 +3,14 @@ using namespace std;
 
 int main() {
 
+<<<<<<< HEAD
+	cout << "Hola mauu ";
 
+=======
+	cout << "Hola Mundo";
+>>>>>>> bd941fc8b1249bd123cc06a576466fef85cf1bad
+
+	//hola hola
 
 	system("pause");
 }
